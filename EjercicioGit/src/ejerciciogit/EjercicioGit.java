4424ec6da25ba4317d011ab4850bc1eaeed34e97
Daniel <Daniel@LAPTOP-1T751ON3>
2020-12-15 19:33:11 +0100

    Se ha creado la rama clasenueva.

--- EjercicioGit/src/ejerciciogit/EjercicioGit.java
+++ EjercicioGit/src/ejerciciogit/EjercicioGit.java
@@ -18,6 +18,7 @@ public class EjercicioGit {
         // TODO code application logic here
         System.out.println("Control");
         System.out.println("de Versiones");
+        
     }
     
 }
